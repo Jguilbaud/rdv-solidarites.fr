e4b2cf9def0e4dfb3708140426d173c937d8d3b5
Victor Mours <victor.mours@beta.gouv.fr>
2022-11-07 12:02:33 +0100

    Avoid running factory spec when running a single test

--- spec/factory_specs/factories_spec.rb
+++ spec/factory_specs/factories_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# This file is not in spec/factory to avoid running this test everytime we run another spec
 FactoryBot.factories.each do |factory|
   describe "The #{factory.name} factory" do
     it "is valid" do
