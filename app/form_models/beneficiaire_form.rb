5064b931586505fa343b6df5162c9e6a405c9111
Victor Mours <victor.mours@beta.gouv.fr>
2022-11-10 18:02:54 +0100

    improve error message

--- app/form_models/beneficiaire_form.rb
+++ app/form_models/beneficiaire_form.rb
@@ -20,7 +20,7 @@ class BeneficiaireForm
     return if ignore_benign_errors
 
     if email.blank? && phone_number.blank?
-      add_benign_error("Aucune notification ne sera envoyée au bénéficiaire")
+      add_benign_error("Sans email ni numéro de téléphone, aucune notification ne sera envoyée au bénéficiaire")
     end
   end
 end
