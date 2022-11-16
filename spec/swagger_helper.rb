5f4ffe19a4eb45e6ba5ec0dcd2c938489f80c068
sebastiencarceles <sebastien.carceles@gmail.com>
2022-11-16 08:06:45 +0100

    DRY response 403

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -395,7 +395,7 @@ RSpec.configure do |config|
             },
             required: %w[errors],
           },
-          error_unauthorized: {
+          error_forbidden: {
             type: "object",
             properties: {
               errors: {
